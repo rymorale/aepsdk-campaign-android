8897a1d7ce03e7600e66fe1655f5cd7e055b6cd2
Ryan Morales <rymorale@adobe.com>
2023-02-15 13:49:40 -0800

    bump version to 2.0.2

--- code/campaign/src/test/java/com/adobe/marketing/mobile/campaign/CampaignExtensionTests.java
+++ code/campaign/src/test/java/com/adobe/marketing/mobile/campaign/CampaignExtensionTests.java
@@ -293,7 +293,7 @@ public class CampaignExtensionTests {
         String version = campaignExtension.getVersion();
 
         // verify
-        assertEquals("2.0.1", version);
+        assertEquals("2.0.2", version);
     }
 
     @Test
