8949ded1bbec0d0fc1759942e5f5880d25f55457
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-03-05 14:30:49 -0800

    Handle null connection on no internet

--- code/campaign/src/main/java/com/adobe/marketing/mobile/campaign/CampaignRulesDownloader.java
+++ code/campaign/src/main/java/com/adobe/marketing/mobile/campaign/CampaignRulesDownloader.java
@@ -123,6 +123,14 @@ class CampaignRulesDownloader {
         networkService.connectAsync(
                 networkRequest,
                 httpConnecting -> {
+                    if (httpConnecting == null) {
+                        Log.warning(
+                                CampaignConstants.LOG_TAG,
+                                SELF_TAG,
+                                "loadRulesFromUrl - No internet connection. Unable to download rules.",
+                                url);
+                        return;
+                    }
                     onRulesDownloaded(url, httpConnecting);
                 });
     }
