f606eb76a319d5c4774cbb1d97e2674318121b09
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-03-08 14:50:56 -0800

    updating the release dependency version

--- code/testapps/testapp/build.gradle.kts
+++ code/testapps/testapp/build.gradle.kts
@@ -37,19 +37,10 @@ android {
 dependencies {
     implementation(project(":campaign"))
     implementation("com.adobe.marketing.mobile:core:3.0.0-beta.1-SNAPSHOT")
-    implementation("com.adobe.marketing.mobile:lifecycle:3.0.0-beta.1-SNAPSHOT") {
-        exclude(group = "com.adobe.marketing.mobile", module = "core")
-    }
-    implementation("com.adobe.marketing.mobile:identity:3.0.0-beta.1-SNAPSHOT") {
-        exclude(group = "com.adobe.marketing.mobile", module = "core")
-    }
-    implementation("com.adobe.marketing.mobile:signal:3.0.0-beta.1-SNAPSHOT") {
-        exclude(group = "com.adobe.marketing.mobile", module = "core")
-    }
-    implementation("com.adobe.marketing.mobile:userprofile:3.0.0-beta.1-SNAPSHOT") {
-        exclude(group = "com.adobe.marketing.mobile", module = "core")
-    }
-
+    implementation("com.adobe.marketing.mobile:lifecycle:3.0.0-beta.1-SNAPSHOT")
+    implementation("com.adobe.marketing.mobile:identity:3.0.0-beta.1-SNAPSHOT")
+    implementation("com.adobe.marketing.mobile:signal:3.0.0-beta.1-SNAPSHOT")
+    implementation("com.adobe.marketing.mobile:userprofile:3.0.0-beta.1-SNAPSHOT")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
